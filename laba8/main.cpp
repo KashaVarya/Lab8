42684e2726feed30025317a72047aa85ff5ccb0f
Alexander <exirtt@gmail.com>
2016-05-28 17:46:58 +0300

    added cpp file

---
+++ laba8/main.cpp
@@ -0,0 +1,18 @@
+#include<windows.h>
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    WIN32_FIND_DATA FindFileData;
+    HANDLE hf;
+    hf=FindFirstFile(L"c:\\test\\test2\\*", &FindFileData);    // вывод названий файлов по указаному пути
+    if (hf!=INVALID_HANDLE_VALUE){
+        do{
+            wcout << FindFileData.cFileName << "\n";
+        }
+        while (FindNextFile(hf,&FindFileData)!=0);
+        FindClose(hf);
+    }
+    return 0;
+}
