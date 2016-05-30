83f23702e63cb844cb3f88b269a9e8cfdaa2606c
Alexander <exirtt@gmail.com>
2016-05-30 11:19:06 +0300

    new function

--- laba8/main.cpp
+++ laba8/main.cpp
@@ -34,14 +34,49 @@ int main()
         FindClose(hf);
     }
     cout << s[2] << endl << s[3] << endl;
+    // 11:13 30.05.2016
+    string str1 = adr;
+    str1.erase(str1.length()-1, 1);
+    string str2;
+    for(int i = 0; i < 2; i++)
+    {
+        str2 = str1 + s[2+i]; // put k faylam + nazvaniye faylov s massiva
+     // cout << str2 << endl;
+        ifstream fin(str2, ios_base::in);
+    //  fin.seekg(0, ios::beg);
+        if(!fin.is_open())
+        {
+            cout << "\nFile don't open";
+            return 0;
+        }
+        string str_for_audio; // stroka dlya zapisi
+     // string str_for_audio2;
+        getline(fin, str_for_audio);
+     // getline(fin, str_for_audio2);
+     // str_for_audio.insert(str_for_audio.length(), str_for_audio2, 0, str_for_audio2.length());
 
-    string str1;
+        fin.close();
+        str_for_audio.erase(str_for_audio.find("TIT2")+22, str_for_audio.length());  // obrezayem po poslednemu tegu, inache zapihivaet ves fayl v stroku
+    //  str_for_audio.push_back('$');
+    //  cout << str_for_audio.length();
+    //  cout << endl << "Pervaya stroka: "<< str_for_audio << endl;
+        string album = str_for_audio.substr(21, 4);   // nahodim po simvolam i vivodim
+        cout << "Albom: " << album << endl;
+        string nazvaniye = str_for_audio.substr(67, 11);
+        cout << "Nazvaniye: " << nazvaniye << endl;
+        string ispolnitel = str_for_audio.substr(50, 7);
+        cout << "Ispolnitel: " << ispolnitel << endl;
+        string god = str_for_audio.substr(36, 4);
+        cout << "God: " << god << endl;
+    }
+    //
+    /*string str1;
     ifstream fin;
     fin.open(s[3]);
     fin >> str1;
     cout << str1;
 
-    fin.close();
+    fin.close();*/
 
 
     return 0;
