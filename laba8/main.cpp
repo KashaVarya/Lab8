174ed2130ac1337ee8011739177f147dc9c5143c
varya <varyax@yandex.ru>
2016-05-31 08:35:26 +0300

    redaktirovaniye

--- laba8/main.cpp
+++ laba8/main.cpp
@@ -8,22 +8,21 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_CTYPE, "rus");
-    //???????????? ?????? ???? ? ?????
-    cout << "??????? ???? ? ?????: ";
+    //Polzovatel vvodit put k papke
+    cout << "Vvedite put k papke: ";
     char adr[80];
-    cin >> adr;   //d:\\AFILE\\* (??? ????)
+    cin >> adr;   //d:\\AFILE\\* (moi put)
 
-    //???????? ?????? ?????? ? ????????? ?????
+    //Poluchaem spisok faylov v ukazannoi papke
     WIN32_FIND_DATAA FindFileData;
     HANDLE hf;
 
    // hf=FindFirstFileA("c:\\test\\test2\\*", &FindFileData);
    // hf=FindFirstFileA("d:\\AFILE\\*", &FindFileData);
 
-    hf=FindFirstFileA(adr, &FindFileData);  // ????????? ????
+    hf=FindFirstFileA(adr, &FindFileData);  // ukazivaem put
 
-    string s[10]; //?????? ? ?????????? ?????? ? ?????
+    string s[10]; //massiv s nazvaniyami faylov v papke
     int i=0;
     if (hf!=INVALID_HANDLE_VALUE){
         do{
@@ -34,6 +33,7 @@ int main()
         FindClose(hf);
     }
     cout << s[2] << endl << s[3] << endl;
+
     // 11:13 30.05.2016
     string str1 = adr;
     str1.erase(str1.length()-1, 1);
@@ -56,7 +56,10 @@ int main()
      // str_for_audio.insert(str_for_audio.length(), str_for_audio2, 0, str_for_audio2.length());
 
         fin.close();
-        str_for_audio.erase(str_for_audio.find("TIT2")+22, str_for_audio.length());  // obrezayem po poslednemu tegu, inache zapihivaet ves fayl v stroku
+
+    //  nichego ne obrezaem, chtobi posle redaktirovaniya vcyu stroku obratno zapisat
+
+    //  str_for_audio.erase(str_for_audio.find("TIT2")+22, str_for_audio.length());  // obrezayem po poslednemu tegu, inache zapihivaet ves fayl v stroku
     //  str_for_audio.push_back('$');
     //  cout << str_for_audio.length();
     //  cout << endl << "Pervaya stroka: "<< str_for_audio << endl;
@@ -67,9 +70,32 @@ int main()
         string ispolnitel = str_for_audio.substr(50, 7);
         cout << "Ispolnitel: " << ispolnitel << endl;
         string god = str_for_audio.substr(36, 4);
-        cout << "God: " << god << endl;
+        cout << "God: " << god << endl << endl;
+
+        //redaktiruem
+        cout << "Xotite redaktirovat?(1- da, 0- net): ";
+        int ans;
+        cin >> ans;
+        if(ans == 1) {
+            cout << "Albom: ";
+            cin >> album;
+            str_for_audio.replace(21, 4, album);
+            cout << "Nazvaniye: ";
+            cin >> nazvaniye;
+            str_for_audio.replace(67, 11, nazvaniye);
+            cout << "Ispolnitel: ";
+            cin >> ispolnitel;
+            str_for_audio.replace(50, 7, ispolnitel);
+            cout << "God: ";
+            cin >> god;
+            str_for_audio.replace(36, 4, god);
+        }
+        ofstream fout(str2);
+        fout << str_for_audio;
+        fout.close();
     }
-    //
+
+
     /*string str1;
     ifstream fin;
     fin.open(s[3]);
